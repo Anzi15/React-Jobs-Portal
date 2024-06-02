63d0a38682ab7b5cd3370a081b63c1f2dee5603f
Lemme.code <djam4343@gmail.com>
2024-06-02 10:09:14 +0500

    @core/pages individual page added

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -30,7 +30,7 @@ const JobListing = ({job}) => {
           {job.location}
         </div>
         <Link
-          to={`/jobs/${job.id}`}
+          to={`/job/${job.id}`}
           className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
           Read More
